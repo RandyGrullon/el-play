119c0ccf48c772c1526bd25791779e0e2fc37988
Randy <randy6grullon@gmail.com>
2025-11-28 20:11:43 -0400

    feat: add schedule controller to fetch and cache MLB schedule data using Santo Domingo time.

--- api/controllers/scheduleController.js
+++ api/controllers/scheduleController.js
@@ -4,12 +4,16 @@ const { TTL } = require('../config/constants');
 
 const getSchedule = async (req, res, next) => {
     try {
-        const today = new Date();
-        const nextWeek = new Date();
-        nextWeek.setDate(today.getDate() + 7);
+        // Use Santo Domingo time to determine "today"
+        const options = { timeZone: 'America/Santo_Domingo', year: 'numeric', month: '2-digit', day: '2-digit' };
+        const formatter = new Intl.DateTimeFormat('en-CA', options); // Returns YYYY-MM-DD
 
-        const startDate = today.toISOString().split('T')[0];
-        const endDate = nextWeek.toISOString().split('T')[0];
+        const today = new Date();
+        const startDate = formatter.format(today);
+
+        const nextWeek = new Date(today);
+        nextWeek.setDate(today.getDate() + 7);
+        const endDate = formatter.format(nextWeek);
         const cacheKey = `schedule_${startDate}_${endDate}`;
 
         const data = await fetchWithCache(cacheKey, () => fetchScheduleData(startDate, endDate), TTL.SCHEDULE);
